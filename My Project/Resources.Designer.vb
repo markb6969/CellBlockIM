888a6aae2c9162996ce61ff7d8a759836fdc862a
Mark Briones <brionesmark63@gmail.com>
2024-11-21 21:32:37 +0800

    Finished About

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -123,6 +123,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property BBRB_Cebu1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("BBRB-Cebu1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property cellblock_central__1_() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("cellblock-central (1)", resourceCulture)
